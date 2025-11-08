d29399efb75105c52e0f62ef516a97c2c749300c
colaaaaaa123 <150669765+colaaaaaa123@users.noreply.github.com>
2025-11-08 20:51:06 +0800

    添加项目文件。

---
+++ SetCPUAffinity/dllmain.cpp
@@ -0,0 +1,23 @@
+﻿#include <Windows.h>
+
+static inline void Log(const char* s) { OutputDebugStringA(s); OutputDebugStringA("\n"); }
+
+BOOL APIENTRY DllMain(HINSTANCE h, DWORD reason, LPVOID) {
+    if (reason == DLL_PROCESS_ATTACH) {
+        DisableThreadLibraryCalls(h);
+
+        DWORD_PTR processMask = 0, systemMask = 0;
+        if (GetProcessAffinityMask(GetCurrentProcess(), &processMask, &systemMask)) {
+            if (!SetProcessAffinityMask(GetCurrentProcess(), systemMask)) {
+                Log("Error: SetProcessAffinityMask failed.");
+            }
+            else {
+                Log("SetProcessAffinityMask succeeded.");
+            }
+        }
+        else {
+            Log("Error: GetProcessAffinityMask failed.");
+        }
+    }
+    return TRUE;
+}
